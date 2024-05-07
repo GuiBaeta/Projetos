074e9ccd6782cc110195e2ba63a3ad6f8a3f0099
GuiBaeta <guilhermebaetapena@gmail.com>
2024-05-07 17:37:05 -0300

    Classe Position

---
+++ Udemy_NelioAlves/SistemaJogoDeXadrez/src/br/gbaeta/com/application/Program.java
@@ -0,0 +1,16 @@
+package br.gbaeta.com.application;
+
+import br.gbaeta.com.boardgame.Position;
+
+/**
+ * @author GuilhermeBaeta
+ */
+public class Program {
+
+	public static void main(String[] args) {
+		 Position pos = new Position(3, 5);
+		 System.out.println(pos);
+
+	}
+
+}
