4f0e346157d8654b03c37e02994e72ff156b311a
GuiBaeta <guilhermebaetapena@gmail.com>
2024-05-26 16:14:21 -0300

    Fix - melhor tratamento para promoção

--- Udemy_NelioAlves/SistemaJogoDeXadrez/src/br/gbaeta/com/chess/ChessMatch.java
+++ Udemy_NelioAlves/SistemaJogoDeXadrez/src/br/gbaeta/com/chess/ChessMatch.java
@@ -3,7 +3,6 @@
  */
 package br.gbaeta.com.chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -95,6 +94,7 @@ public class ChessMatch {
 
 		ChessPiece movedPiece = (ChessPiece) board.piece(target);
 		
+		// #specialmove promotion
 		promoted = null;
 		if (movedPiece instanceof Pawn) {
 			if ((movedPiece.getColor() == Color.WHITE && target.getRow() == 0) || (movedPiece.getColor() == Color.BLACK && target.getRow() == 7)) {
@@ -126,7 +126,7 @@ public class ChessMatch {
 			throw new IllegalStateException("There is no piece to be promoted");	
 		}
 		if (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
-			throw new InvalidParameterException("Invalid type for promotion");
+			return promoted;
 		}
 		
 		Position pos = promoted.getChessPosition().toPosition();
