f990cd15f470455e75b9db18472b380595477e5a
GuiBaeta <guilhermebaetapena@gmail.com>
2024-07-02 18:48:51 -0300

    Adicionando os projetos a pasta projetos

---
+++ Ebac/SegundoProjeto/src/main/java/br/com/gbaeta/dao/IClienteDAO.java
@@ -0,0 +1,9 @@
+package br.com.gbaeta.dao;
+
+import br.com.gbaeta.dao.generic.IGenericDAO;
+import br.com.gbaeta.domain.Cliente;
+
+public interface IClienteDAO extends IGenericDAO<Cliente> {
+
+
+}
